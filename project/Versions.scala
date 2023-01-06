e47d22f34ae66532bb73fbee0c64b8d3561efbec
Carlos Martin <cmarting@gmail.com>
2023-01-06 10:06:01 +0100

    * upgrade scalatest

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,7 +4,7 @@ object Versions {
   val json4s     = "4.0.6"
   val justSemver = "0.6.0"
   val logback    = "1.4.5"
-  val scalatest  = "3.2.14"
+  val scalatest  = "3.2.15"
   val sttp       = "3.8.6"
   val zio        = "2.0.5"
   val zioConfig  = "3.0.6"
