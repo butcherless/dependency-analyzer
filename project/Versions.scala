72b3ff82f7c0c968c78399540d12b01889f3ff69
Carlos Martin <cmarting@gmail.com>
2023-01-10 23:09:34 +0100

    * upgrade sttp client

--- project/Versions.scala
+++ project/Versions.scala
@@ -5,7 +5,7 @@ object Versions {
   val justSemver = "0.6.0"
   val logback    = "1.4.5"
   val scalatest  = "3.2.15"
-  val sttp       = "3.8.7"
+  val sttp       = "3.8.8"
   val zio        = "2.0.5"
   val zioConfig  = "3.0.7"
   val zioLogging = "2.1.7"
