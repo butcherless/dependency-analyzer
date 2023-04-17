0e236b6a15c5e26f531115d03d3cac4d8d37d702
Carlos Martin <cmarting@gmail.com>
2023-04-17 18:04:09 +0200

    * upgrade libs

--- project/Versions.scala
+++ project/Versions.scala
@@ -7,7 +7,7 @@ object Versions {
   val scalatest  = "3.2.15"
   val sttp       = "4.0.0-M1"
   val ulid       = "23.4.2"
-  val zio        = "2.0.12"
+  val zio        = "2.0.13"
   val zioConfig  = "3.0.7"
   val zioLogging = "2.1.12"
   val zioPrelude = "1.0.0-RC18"
