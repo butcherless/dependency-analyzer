72ac4d00e8243768e633afd22e6bab479fca49ff
Carlos Martin <cmarting@gmail.com>
2023-08-09 19:29:17 +0200

    refactor code, add comments, add data generator, scheduling and delay

--- src/main/scala/com/cmartin/utils/poc/CategoryPoc.scala
+++ src/main/scala/com/cmartin/utils/poc/CategoryPoc.scala
@@ -16,7 +16,7 @@ object CategoryPoc
   // aliases to aid type researching
   type RepoError = String
   type Group     = String
-  type Item      = String
+  type Product   = String
 
   // repository data simulation
   object Data {
@@ -33,7 +33,7 @@ object CategoryPoc
 
   trait CategoryRepository {
     def findAllKeys(): ZStream[Any, RepoError, Group]
-    def findAllKeyValues(key: String): ZStream[Any, RepoError, Item]
+    def findAllKeyValues(key: String): ZStream[Any, RepoError, Product]
   }
 
   case class InMemoryCategoryRepository() extends CategoryRepository {
@@ -41,46 +41,86 @@ object CategoryPoc
     override def findAllKeys(): ZStream[Any, RepoError, Group] =
       ZStream.fromIterable(Data.categories.keys)
 
-    override def findAllKeyValues(key: String): ZStream[Any, RepoError, Item] =
+    override def findAllKeyValues(key: String): ZStream[Any, RepoError, Product] =
+      generateFamily(key)
+
+    /*
       ZStream.fromIterable(
         Data.categories.get(key)
-          .fold(List.empty[Item])(identity)
+          .fold(List.empty[Product])(identity)
       )
+     */
 
   }
 
-  /** Retrieves all items from all categories.
+  /** Retrieves all Products from all categories.
     *   - retrieves all categories
-    *   - retrieves all items from each category
-    *   - maps to (group, item) tuple
+    *   - retrieves all Products from each category
+    *   - maps to (group, product) tuple
     *   - groups by group (key)
-    *   - resolve item Stream to List
-    *   - maps to (group, item-list)
-    *   - resolve (group, item-list) Stream to Map
+    *   - resolve product stream to list
+    *   - maps to (group, product-list)
+    *   - resolve (group, product-list) stream to map
+    *
     * @param categoryRepository
     *   dummy category repository
     */
   case class ZioFindCategoriesUseCase(categoryRepository: CategoryRepository) {
-    def execute(): IO[RepoError, Map[Group, List[Item]]] =
+    private def streamToList(products: ZStream[Any, RepoError, Product]): ZIO[Any, RepoError, List[Product]] =
+      products.runFold(List.empty[Product])((list, product) => list :+ product)
+
+    def execute(): IO[RepoError, Map[Group, List[Product]]] =
       categoryRepository.findAllKeys()
-        .flatMap(group =>
-          categoryRepository.findAllKeyValues(group)
-            .map(item => (group, item))
-        ).groupBy { case (group, item) => ZIO.succeed(group, item) } { case (group, items) =>
-          ZStream.fromZIO(items.runFold(List.empty[Item])((list, item) => list :+ item))
-            .map(items => (group, items))
-        }.runFold(Map.empty[Group, List[Item]]) { case (map, (group, item)) => map + (group -> item) }
+        .flatMapPar(3)(groupKey =>
+          categoryRepository.findAllKeyValues(groupKey)
+            .map(product => (groupKey, product))
+            .tap(a => ZIO.log(s"elem: $a"))
+        // .tap(a => Console.printLine(s"elem: $a").orDie)
+        ).groupBy { case (group, product) =>
+          ZIO.succeed(group, product)
+        } { case (group, products) =>
+          ZStream.fromZIO(streamToList(products))
+            .map(products => (group, products))
+        }.runFold(Map.empty[Group, List[Product]]) { case (map, (group, product)) =>
+          map + (group -> product)
+        }
 
   }
 
   val repo    = InMemoryCategoryRepository()
   val useCase = ZioFindCategoriesUseCase(repo)
 
+  def generateRandomInt() =
+    scala.util.Random.between(5, 100 + 1)
+
+  def generateFamily(name: String): ZStream[Any, RepoError, Product] =
+    ZStream.iterate(1)(_ + 1).takeWhile(_ <= 100)
+      .schedule(Schedule.spaced(generateRandomInt().milliseconds))
+      .map(a => s"$name-$a")
+      // .foreach(a => Console.printLine(s"element: $a"))
+      .mapError(e => e.toString())
+
   override def run =
     for {
       _   <- ZIO.log("hello categories")
       map <- useCase.execute()
       _   <- ZIO.log(s"category map: ${map.mkString("\n\n", "\n", "\n\n")}")
+      // _   <- generateFamily("number")
     } yield ()
 
 }
+
+/*
+
+( c1, p11 ) + ( c1, p12 ) + ( c1, p13 ) lento  4s
+( c2, p21 ) + ( c2, p22 )  1s
+( c3, p31 ) + ( c3, p31 ) + ( c3, p3 ) 2s
+
+----
+-
+--
+
+ ( c1, p11 ) + ( c3, p31 ) + ( c2, p21 ) + ( c2, p22 ) ...... déjalo fluir
+ groupByKey( funcion )
+
+ */
