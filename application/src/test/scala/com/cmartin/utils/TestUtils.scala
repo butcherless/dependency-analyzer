ba6c2267a140b2ce8122172220da70feeb339e8a
Carlos Martin <cmarting@gmail.com>
2023-08-27 13:03:07 +0200

    add SemVer validator POC

---
+++ application/src/test/scala/com/cmartin/utils/TestUtils.scala
@@ -0,0 +1,13 @@
+package com.cmartin.utils
+
+import zio.{Unsafe, ZIO}
+import zio.Runtime.{default => runtime}
+
+object TestUtils {
+
+  def run[E, A](program: ZIO[Any, E, A]) =
+    Unsafe.unsafe { implicit u =>
+      runtime.unsafe.run(program).getOrThrowFiberFailure()
+    }
+
+}
---
+++ application/src/test/scala/com/cmartin/utils/poc/SemverValidatorSpec.scala
@@ -0,0 +1,53 @@
+package com.cmartin.utils.poc
+
+import com.cmartin.utils.TestUtils
+import just.semver.SemVer
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import zio.{IO, ZIO}
+
+import scala.util.matching.Regex
+
+class SemverValidatorSpec extends AnyFlatSpec
+    with Matchers {
+
+  behavior of "ZStreamPocSpec"
+  private val pattern: Regex =
+    "(^[^:A-Z]+):([^:A-Z]+):([^:]+$)".r
+
+  val nettyLine = "io.netty:netty-buffer:4.1.95.Final"
+  val slf4jLine = "org.slf4j:slf4j-api:2.0.0-alpha1"
+
+  def extractVersion(line: String): IO[String, String] =
+    line match {
+      case pattern(_, _, v) => ZIO.succeed(v)
+      case _                => ZIO.fail("invalid version")
+    }
+
+  def validate(version: String) =
+    SemVer.parse(version).fold(
+      _ => ZIO.succeed(false),
+      _ => ZIO.succeed(true)
+    )
+
+  it should s"validate $nettyLine" in {
+    val prog = for {
+      version <- extractVersion(nettyLine)
+      _       <- zio.Console.printLine(s"$version")
+      result  <- validate(version)
+    } yield result
+
+    TestUtils.run(prog) shouldBe false
+  }
+
+  it should s"validate $slf4jLine" in {
+    val prog = for {
+      version <- extractVersion(slf4jLine)
+      _       <- zio.Console.printLine(s"$version")
+      result  <- validate(version)
+    } yield result
+
+    TestUtils.run(prog) shouldBe true
+  }
+
+}
