e847770ebc0a6a51388981ea413e5349c3adedb4
Carlos Martin <cmarting@gmail.com>
2023-09-29 10:10:00 +0200

    upgrade dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -6,9 +6,9 @@ object Versions {
   val logback    = "1.4.11"
   val scalatest  = "3.2.17"
   val sttp       = "4.0.0-M5"
-  val ulid       = "23.9.2"
+  val ulid       = "23.9.3"
   val zio        = "2.0.18"
   val zioConfig  = "4.0.0-RC16"
-  val zioKafka   = "2.4.2"
+  val zioKafka   = "2.4.3"
   val zioLogging = "2.1.14"
 }
