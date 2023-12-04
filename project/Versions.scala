91f6e34baf3f5d1d671a8a221cf1081a2050182c
Carlos Martin <cmarting@gmail.com>
2023-12-04 18:32:48 +0100

    upgrade dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,11 +1,11 @@
 object Versions {
   val config     = "1.4.2"
   val figlet4s   = "0.3.2"
-  val json4s     = "4.0.6"
+  val json4s     = "4.0.7"
   val justSemver = "0.13.0"
   val logback    = "1.4.14"
   val scalatest  = "3.2.17"
-  val sttp       = "4.0.0-M6"
+  val sttp       = "4.0.0-M8"
   val ulid       = "23.11.3"
   val zio        = "2.0.19"
   val zioConfig  = "4.0.0-RC16"
