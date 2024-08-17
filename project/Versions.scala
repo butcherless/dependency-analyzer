6271d550184c9fbdc448fb610f9905e0fef89ab9
Carlos Martin <cmarting@gmail.com>
2024-08-17 20:36:04 +0200

    upgrade deps upgrade scala lang

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,10 +1,10 @@
 object Versions {
-  val scala      = "3.4.2"
+  val scala      = "3.5.0"
   val config     = "1.4.2"
   val figlet4s   = "0.3.2"
   val json4s     = "4.0.7"
   val justSemver = "0.13.0"
-  val logback    = "1.5.6"
+  val logback    = "1.5.7"
   val scalatest  = "3.2.19"
   val scraper    = "3.1.1"
   val sttp       = "4.0.0-M17"
