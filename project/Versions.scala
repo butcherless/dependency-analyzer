e0551758138a486aeda9664cd8cd4b6f77ef76f1
Carlos Martin <cmarting@gmail.com>
2024-12-23 22:53:00 +0100

    upgrade dependencies

--- project/Versions.scala
+++ project/Versions.scala
@@ -4,7 +4,7 @@ object Versions {
   val figlet4s   = "0.3.2"
   val json4s     = "4.0.7"
   val justSemver = "1.1.0"
-  val logback    = "1.5.14"
+  val logback    = "1.5.15"
   val scalatest  = "3.2.19"
   val scraper    = "3.1.2"
   val sttp       = "4.0.0-M20"
