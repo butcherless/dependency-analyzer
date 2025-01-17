5f5f0fff34ff49e4157826f145ab0a8453a7b50e
Carlos Martin <cmarting@gmail.com>
2025-01-17 15:42:33 +0100

    Update dependency versions

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,10 +1,10 @@
 object Versions {
-  val scala      = "3.6.2"
+  val scala      = "3.6.3"
   val config     = "1.4.2"
   val figlet4s   = "0.3.2"
   val json4s     = "4.0.7"
   val justSemver = "1.1.0"
-  val logback    = "1.5.15"
+  val logback    = "1.5.16"
   val scalatest  = "3.2.19"
   val scraper    = "3.1.2"
   val sttp       = "4.0.0-M24"
